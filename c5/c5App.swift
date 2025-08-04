e2e725f707e461e0567ddea390014433ec618343
alex wells <wfc.ltd2me.com>
2025-08-04 08:33:01 +0100

    Initial Commit

---
+++ c5/c5App.swift
@@ -0,0 +1,17 @@
+//
+//  c5App.swift
+//  c5
+//
+//  Created by Alex Wells on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct c5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
